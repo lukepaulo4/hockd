60a833275fe5714d6b2a399fe7fb61e3e93bd046
Luke Paulo <lukepaulo4@gmail.com>
2016-08-31 00:19:34 -0700

    Tab Bar & Nav Controllers with Classes Set Up

---
+++ Hockd iOS App/Hockd iOS App/User.h
@@ -0,0 +1,16 @@
+//
+//  User.h
+//  Hockd iOS App
+//
+//  Created by Luke Paulo on 8/31/16.
+//  Copyright © 2016 HOCKD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic, strong) NSString *idNumber;
+
+
+@end
---
+++ Hockd iOS App/Hockd iOS App/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  Hockd iOS App
+//
+//  Created by Luke Paulo on 8/31/16.
+//  Copyright © 2016 HOCKD. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
