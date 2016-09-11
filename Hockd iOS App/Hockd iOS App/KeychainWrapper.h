5489a45e848ec3291f8b572ddfad4ee5a77ce12d
Luke Paulo <lukepaulo4@gmail.com>
2016-09-11 14:03:43 -0700

    made some ups on master on accident

---
+++ Hockd iOS App/Hockd iOS App/KeychainWrapper.h
@@ -0,0 +1,16 @@
+//
+//  KeychainWrapper.h
+//  Hockd iOS App
+//
+//  Created by Luke Paulo on 9/11/16.
+//  Copyright © 2016 HOCKD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeychainWrapper : NSObject
+
+//initialize the initializers
+
+
+@end
