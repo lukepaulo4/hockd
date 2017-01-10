7fc877b3edbddd36dcf6dbe97b9053c169ab5d7d
Luke Paulo <lukepaulo4@gmail.com>
2017-01-10 01:29:52 -0800

    trying to add basic images to an array to build the table cells and failing miserably

--- Hockd iOS App/Hockd iOS App/MyItemsTableViewController.m
+++ Hockd iOS App/Hockd iOS App/MyItemsTableViewController.m
@@ -14,6 +14,7 @@
 
 @interface MyItemsTableViewController ()
 
+@property (nonatomic, strong) NSMutableArray *images;
 
 @end
 
@@ -22,7 +23,8 @@
 - (id)initWithStyle:(UITableViewStyle)style {
     self = [super initWithStyle:style];
     if (self) {
-        
+        //create the empty array for the images
+        self.images = [NSMutableArray array];
     }
     return self;
 }
@@ -31,7 +33,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.tableView registerClass:[ItemTVCell class] forCellReuseIdentifier:@"itemTableCell"];
+    //add the images to the array
+    for (int i = 1; i <= 10; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"%d.jpg", i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        if (image) {
+            [self.images addObject:image];
+        }
+    }
+    
+    NSLog(@"array from table vc view did load is %@", self.images);
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"imageCell"];
     
     
     
@@ -47,27 +60,54 @@
 //Need to present all of our images as rows in this table.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
-    return [DataSource sharedInstance].items.count;
+    //return [DataSource sharedInstance].items.count;
+    
+    return self.images.count;
 }
 
 
 //Required method and most important. Returns a prepped and ready UITableViewCell instance to the table view to display at a given location. This method is called every time a new row is about to appear on screen whether scrolling up or down.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    //1
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
+    
+    //2
+    static NSInteger imageViewTag = 1234;
+    UIImageView *imageView = (UIImageView*)[cell.contentView viewWithTag:imageViewTag];
+    
+    //3
+    if (!imageView) {
+        //This is a new cell, it doesn't have an image yet
+        imageView = [[UIImageView alloc] init];
+        imageView.contentMode = UIViewContentModeScaleToFill;
+        
+        imageView.frame = cell.contentView.bounds;
+        imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        
+        imageView.tag = imageViewTag;
+        [cell.contentView addSubview:imageView];
+    }
+    
+    UIImage *image = self.images[indexPath.row];
+    imageView.image = image;
+    
+    
+    
+    /*
     ItemTVCell *cell = [tableView dequeueReusableCellWithIdentifier:@"itemTableCell" forIndexPath:indexPath];
     
     if (cell == nil) {
         cell = [[ItemTVCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"itemTableCell"];
     }
     
-    
-    
     //Now configure the cell
-   // cell.loanDesiredLabel.text =  objectAtIndex:[indexPath row]];
-   // cell.itemDescriptionLabel.text = [self.item.itemDescription objectAtIndex:[indexPath row]];
+    cell.loanDesiredLabel.text =  objectAtIndex:[indexPath row]];
+    cell.itemDescriptionLabel.text = [self.item.itemDescription objectAtIndex:[indexPath row]];
     
     cell.item = [DataSource sharedInstance].items[indexPath.row];
-
+    */
+    
     
     return cell;
 }
