6c741ea6a05aa257ff697ef8d806319b7bf5d12f
Luke Paulo <lukepaulo4@gmail.com>
2017-01-31 20:03:05 -0700

    full screen for my items working with random data. still need to fix some layout issues and what not.

--- Hockd iOS App/Hockd iOS App/MyItemFullScreenViewController.m
+++ Hockd iOS App/Hockd iOS App/MyItemFullScreenViewController.m
@@ -33,7 +33,8 @@
     //#1 scroll view already created and configured in storyboard. and made as a subview of view. image view should be a subview of the scroll view.
     //self.scrollView.delegate = self;
     
-    //#2 create image view
+    //#2 create image view THIS IS WHAT YOU ACTUALLY USE. UNCOMMENT WHEN THE API IS WORKING AND CAN ACTUALLY TEST
+    /*
     self.imageView.image = self.item.imageOne;
     self.itemDescriptionLabel.text = self.item.itemDescription;
     self.loanAmountLabel.text = self.item.loanDesired;
@@ -43,6 +44,31 @@
     self.modelLabel.text = self.item.modelName;
     self.categoryLabel.text = self.item.category;
     self.otherCommentsLabel.text = self.item.otherComments;
+     */
+    
+    //JUST FILLER STUFF UNTIL WE GET THE API FUNCTIONING!!!!
+    NSString *itemD = @"Macbook Pro with very little wear and tear. Has 800 RAM and a big ass apple dong";
+    NSString *loanA = @"$1200";
+    NSString *cond = @"almost new";
+    NSString *manu = @"apple";
+    NSString *manY = @"2013";
+    NSString *model = @"macbook pro";
+    NSString *cat = @"electronics";
+    NSString *other = @"suck me long time";
+    NSString *imageName = [NSString  stringWithFormat:@"4.jpg"];
+    UIImage *image = [UIImage imageNamed:imageName];
+    
+    
+    self.imageView.image = image;
+    self.itemDescriptionLabel.text = itemD;
+    self.loanAmountLabel.text = loanA;
+    self.conditionLabel.text = cond;
+    self.manufacturerLabel.text = manu;
+    self.yearMadeLabel.text = manY;
+    self.modelLabel.text = model;
+    self.categoryLabel.text = cat;
+    self.otherCommentsLabel.text = other;
+    //end the bullshit here
     
     
 }
